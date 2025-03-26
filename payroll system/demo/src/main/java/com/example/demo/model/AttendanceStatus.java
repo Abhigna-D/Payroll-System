13f414b9dbbf18e279dd996c3f7afa52c1362a56
Abhigna-D <147600624+Abhigna-D@users.noreply.github.com>
2025-03-26 20:36:01 +0530

    Delete payroll system/demo directory

--- payroll system/demo/src/main/java/com/example/demo/model/AttendanceStatus.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model;
-
-public enum AttendanceStatus {
-    PRESENT,
-    ABSENT,
-    SICK_LEAVE,
-    HALF_DAY,
-    WORK_FROM_HOME
-}
--- payroll system/demo/src/main/java/com/example/demo/model/ERole.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model;
-
-public enum ERole {
-    ROLE_EMPLOYEE,
-    ROLE_HR_MANAGER,
-    ROLE_FINANCIAL_OFFICER,
-    ROLE_DEPARTMENT_MANAGER
-} 
